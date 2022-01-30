a5ea5a36880e372622454ac81537b7bdaef76aa0
gyokufoo <tomohisamawatari@gmail.com>
2022-01-30 21:14:30 +0900

    creat set name

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SignIn from './SignIn'
 
 export default () => {
-  return <SignIn></SignIn>;
+  const [name, setName] = useState('');
+  console.log({name});
+  return <SignIn setName={setName}></SignIn>;
 };
