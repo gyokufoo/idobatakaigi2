8e577c6aba0bd6ab583f0e961051ea30ac919d1d
gyokufoo <tomohisamawatari@gmail.com>
2022-01-31 13:52:00 +0900

    apply grid layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Box } from '@mui/system';
+import MessageList from './MessageList';
+import MessageInputField from './MessageInputField';
 
-const Main = (props) => {
-    const {name} = props;
-    return <div>Main{name}</div>
+const Main = ({name}) => {
+    return (
+        <Box
+            sx={{
+                display:'grid',
+                height:'100vh',
+                gridTemplateRows: '1fr auto'
+            }}
+        >
+            <MessageList></MessageList>
+            <MessageInputField name={name}></MessageInputField>
+        </Box>
+    )
 }
 
 export default Main;
