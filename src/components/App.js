640af384da7cb26c8a37bf6cbcd93f184f745196
gyokufoo <tomohisamawatari@gmail.com>
2022-01-31 09:53:54 +0900

    creat config

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React, {useState} from 'react';
 import SignIn from './SignIn';
 import Main from './Main';
+import config from "../config.json";
+
+console.log(config)
 
 export default () => {
   const [name, setName] = useState('');
   console.log({name});
-  if (name===''){
+  if (name==='' && config.signInEnabled === true){
     return <SignIn setName={setName}></SignIn>;
   }else{
     return <Main name={name}></Main>
